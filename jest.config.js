318d8da4135d3e9dee0ac21700dd59dd09429f1e
Le Chen <le.chen@revas.io>
2020-04-29 18:11:17 +0200

    test: collect coverage from vue and ts files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverageFrom: ['**/*.{vue,ts}', '!**/node_modules/**', '!**/coverage/**', '!**/tests/e2e/**'],
   coverageReporters: ['text-summary', 'html', 'json']
 }
